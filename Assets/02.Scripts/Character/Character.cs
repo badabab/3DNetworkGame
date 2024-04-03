cfa56e16b1cfebc6ae87f53becd512f28fa23c54
badabab <ky040723@gmail.com>
2024-04-03 18:13:21 +0900

    플레이어 처치하면 플레이어 점수의 반이 내껄로.. 나머지 반은 점수 오브젝트 흩뿌리기

--- Assets/02.Scripts/Character/Character.cs
+++ Assets/02.Scripts/Character/Character.cs
@@ -2,7 +2,6 @@ using Cinemachine;
 using Photon.Pun;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 [RequireComponent(typeof(CharacterMoveAbility))]
 [RequireComponent(typeof(CharacterRotateAbility))]
@@ -60,6 +59,34 @@ public class Character : MonoBehaviour, IPunObservable, IDamaged
         ExitGames.Client.Photon.Hashtable myHashtable = PhotonNetwork.LocalPlayer.CustomProperties;
         myHashtable["Score"] = 0;
         PhotonNetwork.LocalPlayer.SetCustomProperties(myHashtable);
+    }
+    public void KillScore(int actorNumber)
+    {
+        int killScore = (int)PhotonNetwork.LocalPlayer.CustomProperties["Score"] / 2;
+        ExitGames.Client.Photon.Hashtable playerHashtable = PhotonNetwork.CurrentRoom.GetPlayer(actorNumber).CustomProperties;
+        playerHashtable["Score"] = (int)playerHashtable["Score"] + killScore;
+    }
+    public void DeathItem()
+    {
+        int killScore = (int)PhotonNetwork.LocalPlayer.CustomProperties["Score"] / 2;
+
+        int itemCount = killScore / 100; // ScoreItem1
+        int remainingScore = killScore % 100; // 남은 점수
+        for (int i = 0; i < itemCount; i++)
+        {
+            ItemObjectFactory.Instance.RequestCreate(ItemType.ScoreItem1, transform.position);
+        }
+        itemCount = remainingScore / 50; // ScoreItem2
+        remainingScore %= 50; // 남은 점수
+        for (int i = 0; i < itemCount; i++)
+        {
+            ItemObjectFactory.Instance.RequestCreate(ItemType.ScoreItem2, transform.position);
+        }
+        itemCount = remainingScore / 20; // ScoreItem3
+        for (int i = 0; i < itemCount; i++)
+        {
+            ItemObjectFactory.Instance.RequestCreate(ItemType.ScoreItem3, transform.position);
+        }
     }
     public void AddKillCount (int actorNumber)
     {
@@ -124,6 +151,7 @@ public class Character : MonoBehaviour, IPunObservable, IDamaged
             string nickName = PhotonNetwork.CurrentRoom.GetPlayer(actorNumber).NickName;
             string logMessage = $"\n<color=#32CD32>{nickName}</color>님이 <color=#32CD32>{PhotonView.Owner.NickName}</color>을 <color=red>처치</color>하였습니다.";
             PhotonView.RPC(nameof(AddLog), RpcTarget.All, logMessage);
+            KillScore(actorNumber);
             AddKillCount(actorNumber);
         }
         else
@@ -158,6 +186,7 @@ public class Character : MonoBehaviour, IPunObservable, IDamaged
 
         if (PhotonView.IsMine)
         {
+            DeathItem();
             // 팩토리패턴: 객체 생성과 사용 로직을 분리해서 캡슐화하는 패턴
             int num = UnityEngine.Random.Range(0, 10);
             if (num == 0) // 10%
@@ -170,7 +199,7 @@ public class Character : MonoBehaviour, IPunObservable, IDamaged
             }
             else // 70%
             {
-                int itemCount = UnityEngine.Random.Range(10, 30);
+                int itemCount = UnityEngine.Random.Range(5, 15);
                 for (int i = 0; i < itemCount; i++)
                 {
                     int random = UnityEngine.Random.Range(0, 3);
--- Assets/02.Scripts/Weapon/Weapon.cs
+++ Assets/02.Scripts/Weapon/Weapon.cs
@@ -1,11 +1,34 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     public CharacterAttackAbility MyCharacterAttackAbility;
+    public int Score;
+    private int _currentScore;
 
     private void OnTriggerEnter(Collider other)
     {
         MyCharacterAttackAbility.OnTriggerEnter(other);
+    }
+
+    private void Start()
+    {
+        gameObject.transform.localScale = Vector3.one;
+        Score = (int)PhotonNetwork.LocalPlayer.CustomProperties["Score"];
+        _currentScore = Score;
+    }
+
+    private void Update()
+    {
+        if (_currentScore >= 1000)
+        {
+            SetWeaponScale();
+            _currentScore -= 1000;
+        }
+    }
+    private void SetWeaponScale()
+    {
+        gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
     }
 }
