c96bcfacaee82f8a27c7ac064a9a87dfb2e64ede
badabab <ky040723@gmail.com>
2024-04-09 18:12:42 +0900

    닉네임 바꾸기

--- Assets/02.Scripts/UI/UI_Lobby.cs
+++ Assets/02.Scripts/UI/UI_Lobby.cs
@@ -37,12 +37,20 @@ public class UI_Lobby : MonoBehaviour
 
         PhotonNetwork.NickName = nickname;
 
+        // [ 룸 옵션 설정 ]
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 20;             // 입장 가능한 최대 플레이어 수
         roomOptions.IsVisible = true;            // 로비에서 방 목록에 노출할 것인가?
         roomOptions.IsOpen = true;               // 방이 열려있는 상태인가?
         roomOptions.EmptyRoomTtl = 1000 * 20;    // 비어있는 방 살아있는 시간(TimeToLive)
         // roomOptions.PlayerTtl = 100 * 30;
+        roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable()  // 룸 커스텀 프로퍼티 (플레이어 커스텀 프로퍼티)
+        {
+            {"MasterNickname", nickname}
+        };
+        // 로비에서 공개적으로 표시될 룸 커스텀 프로퍼티의 키를 정의
+        // -> 방을 검색하거나 선택할 때 사용자에게 유용한 정보를 제공하기 위해 사용
+        roomOptions.CustomRoomPropertiesForLobby = new string[] { "MasterNickname" };
 
         PhotonNetwork.JoinOrCreateRoom(roomID, roomOptions, TypedLobby.Default);   // 방이 있다면 입장하고 없다면 만드는 것 
     }
@@ -54,5 +62,14 @@ public class UI_Lobby : MonoBehaviour
         SelectedCharacterType = characterType;
         MaleCharacter.SetActive(SelectedCharacterType == CharacterType.Male);
         FemaleCharacter.SetActive(SelectedCharacterType == CharacterType.Female);
+    }
+
+    public void OnNicknameValueChanged(string newValue)
+    {
+        if (string.IsNullOrEmpty(newValue))
+        {
+            return;
+        }
+        PhotonNetwork.NickName = newValue;
     }
 }
